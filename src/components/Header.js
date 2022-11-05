f12beeee8237c2c894362319e56f58a22e711d0a
Bankk <106103625+BankkRoll@users.noreply.github.com>
2022-11-05 17:02:24 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import '../styles/Header.css';
+import MenuBar from "../components/MenuBar";
+import react_icon from '../images/react_icon.svg'
+import js_icon from '../images/js_icon.svg'
+import html_icon from '../images/html_icon.svg'
+import css_icon from '../images/css_icon.svg'
+import json_icon from '../images/json_icon.svg'
+
+
+function Header() {
+  return ( 
+    <header className="header-container">
+      <nav className="nav-container">
+        <MenuBar 
+          icon={ react_icon }
+          name='Home.jsx'
+          path="/"
+        /> 
+        <MenuBar 
+          icon={ html_icon }
+          name='About.html'
+          path="/about"
+        />
+        <MenuBar 
+          icon={ js_icon }
+          name='Projects.js'
+          path="/projects"
+        />
+        <MenuBar 
+          icon={ json_icon }
+          name='Services.json'
+          path="/recommendation"
+        /> 
+        <MenuBar 
+          icon={ css_icon }
+          name='Contact.css'
+          path="/contact"
+        /> 
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/data/games.js
@@ -0,0 +1,27 @@
+const games = [
+    {
+      name: 'FROOTIEZ SHOOTIEZ Game', 
+      description: 'FROOTIEZ SHOOTIEZ NFT Game', 
+      src: 'frootiezshootiez.png', 
+      site: 'https://itch.io/embed-upload/6783751?color=64ee40', 
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      name: 'Budlist Game', 
+      description: 'Budlist game for wl or discord invite code', 
+      src: 'budlist.png', 
+      site: 'https://frootiez-shootiez.itch.io/budlist', 
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      name: 'Fr00py Land Game', 
+      description: 'Fr00py Land game for a fun expeirnce of two boys NFT', 
+      src: 'froopy.gif', 
+      site: 'https://bankkrolleth.itch.io/fr00pyland', 
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+    }
+    
+  ];
+  
+  export default games;
+  
