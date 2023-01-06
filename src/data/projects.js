bb7b3cd619312136a0ee70e77070359882221876
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-01-06 01:20:33 -0500

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -32,7 +32,7 @@ const projects = [
   {
     name: 'Custom Portfolio', 
     description: 'Need a custom Portfolio?', 
-    src: '6688.png', 
+    src: '6688.', 
     site: 'https://frentech.vercel.app/',
     repository: 'https://github.com/BankkRoll/Development-Portfolio',
     technologies: ['React', 'Next.js','Node.js', 'Chakra'],
