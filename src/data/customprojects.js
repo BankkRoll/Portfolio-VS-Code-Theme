50b00b1ddf987d6c8c3cf0ac989d01a68a9b9f3c
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-01-10 01:39:13 -0500

    Create customprojects.js

---
+++ src/data/customprojects.js
@@ -0,0 +1,99 @@
+const customprojects = [
+    {
+    name: 'Petal Headz', 
+    description: 'Petal Headz NFT', 
+    src: 'Petal.png', 
+    site: 'https://opensea.io/collection/petal-headz', 
+    technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'web3.js', 'Solidity'],
+  },
+  {
+    name: 'Frens Radio Network', 
+    description: 'A Space For All Frens In WEB3! We provide a global platform for all Web3 to pool skills, resources, opportunities for growth and have a great time!', 
+    src: 'frensradio.gif', 
+    site: 'https://frensradio.io', 
+    technologies: ['React', 'Next.js', 'web3.js', 'CSS'],
+  },
+  {
+    name: 'Frens Connect', 
+    description: 'COMING SOON!', 
+    src: 'soon.gif',
+    site: 'https://app.frensradio.io', 
+    technologies: ['React', 'Next.js', 'Node.js', 'web3.js'],
+  },
+    {
+    name: 'Frens Radio NFT Mint', 
+    description: 'Frens Radio NFT minting site!', 
+    src: 'frnmint.png',
+    site: 'https://mint.frensradio.io', 
+    technologies: ['React', 'Next.js', 'Node.js', 'web3.js', 'Tailwind CSS'],
+  },
+  {
+    name: 'Cheddar & Frens', 
+    description: 'Cheddar & Frens are a Web3 sub community of Alien Frens... Building upon our cheesy ideas! Established by like minded frens that share a common trait of cheese heads derived from the NFT Community, Alien Frens.', 
+    src: 'cheddar.gif', 
+    site: 'https://www.cheddarandfrens.com/', 
+    technologies: ['React', 'core-js', 'jQuery'],
+  },
+  {
+    name: 'Frenship Book Company', 
+    description: 'Creating picture books to inspire and guide children in Web 3.0 and the rest of the world. Author ✍️ @Rave4_H - Illustrator 🖌️@jpegjonno', 
+    src: 'frenshipbook.gif', 
+    site: 'https://www.frenshipbookcompany.com/', 
+    technologies: ['React', 'core-js', 'jQuery'],
+  },
+  {
+    name: 'DOINK ME', 
+    description: 'DOINK ME, PFP overlay generator', 
+    src: 'DOINKME.png', 
+    site: 'https://doinkme.xyz/', 
+    technologies: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+  },
+  {
+    name: 'ShrineList Wallet Checker (WL Checker)', 
+    description: 'Custom Wallet Checker for WL spots.', 
+    src: 'wlsad.gif', 
+    technologies: ['React', 'Next.js', 'CSS'],
+  },
+  {
+    name: 'Shrine After Died', 
+    description: 'Shrine After Died is a genesis AI collection by GLR.', 
+    src: 'sad.gif', 
+    technologies: ['React', 'Next.js', 'web3.js', 'HTML', 'CSS'],
+  },
+  {
+    name: 'The Chariot', 
+    description: 'The Chariot NFT', 
+    src: 'chariot.png', 
+    site: 'https://www.thechariotnft.xyz/', 
+    technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'web3.js', 'Solidity'],
+  },
+  {
+    name: 'The Ascended Masters', 
+    description: 'The Ascended Masters NFT', 
+    src: 'am.png', 
+    site: 'https://www.ascendedmasters.xyz/', 
+    technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'web3.js', 'Solidity'],
+  },
+  {
+    name: 'Downers NFT', 
+    description: 'Downers NFT', 
+    src: 'downers.png', 
+    technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'web3.js', 'Solidity'],
+  },
+  {
+    name: 'FROOTIEZ', 
+    description: 'FROOTIEZ NFT', 
+    src: 'frootiez.png', 
+    technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'web3.js', 'Solidity'],
+  },
+  {
+    name: 'Budlist Game', 
+    description: 'Budlist game for wl or discord invite code', 
+    src: 'budlist.png', 
+    site: 'https://bankkrolleth.itch.io/budlist', 
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+  }
+  
+];
+
+export default projects;
