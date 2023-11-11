c5527dedd5297a04bca71d180b5a5f7e2722e25d
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-11-11 10:37:25 -0500

    .

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,6 +1,6 @@
 const contacts = [
-  {social: 'twitter ', href: 'https://twitter.com/bankkroll_eth', text: "/bankkroll_eth"},
-  {social: 'github', href: 'https://github.com/BankkRoll', text: "/BankkRoll"},
+  {social: 'twitter ', href: 'https://twitter.com/bankkroll_eth', text: "@bankkroll_eth"},
+  {social: 'github', href: 'https://github.com/BankkRoll', text: "@BankkRoll"},
   {social: 'email', href: 'mailto:bankkroll.eth@gmail.com', text:'bankkroll.eth@gmail.com'},
   {social: 'discord', href: 'https://discord.com/invite/gN6zG964bj', text:'Discord_Support_Server'},
 ];
